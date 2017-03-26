b1bb3f48e3c8284eb95582b86824262961c5976e
warthur1214 <warthur@163.com>
2017-03-26 16:57:55 +0800

    spring boot redis demo

--- spring-demo/spring-redis/src/main/java/com/warthur/demo/redis/config/RedisCacheConfig.java
+++ spring-demo/spring-redis/src/main/java/com/warthur/demo/redis/config/RedisCacheConfig.java
@@ -1,4 +1,4 @@
-package com.warthur.demo.redis.comfig;
+package com.warthur.demo.redis.config;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
