2fc7231122303255bf3e145182209ea8c3579d10
warthur <warthur@163.com>
2017-03-27 19:20:52 +0800

    shiro jpa junit test mail sending

---
+++ spring-demo/spring-mail/src/main/java/com/warthur/demo/mail/MailApplication.java
@@ -0,0 +1,15 @@
+package com.warthur.demo.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by admin on 2017/3/27.
+ */
+@SpringBootApplication
+public class MailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailApplication.class, args);
+	}
+}
---
+++ spring-demo/spring-mail/src/test/java/com/warthur/demo/mail/AppTest.java
@@ -0,0 +1,76 @@
+package com.warthur.demo.mail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+/**
+ * Created by admin on 2017/3/27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MailApplication.class)
+public class AppTest {
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	/**
+	 * 修改application.properties的用户，才能发送。
+	 */
+	@Test
+	public void sendSimpleEmail(){
+		SimpleMailMessage message = new SimpleMailMessage();
+
+		message.setFrom("warthur@163.com");//发送者.
+		message.setTo("wuyongqiang@chinaubi.com");//接收者.
+		message.setSubject("测试邮件（邮件主题）");//邮件主题.
+		message.setText("这是邮件内容");//邮件内容.
+
+		mailSender.send(message);//发送邮件
+	}
+
+	@Test
+	public void sendAttachmentsEmail() throws MessagingException {
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+		helper.setFrom("warthur@163.com");
+		helper.setTo("wuyongqiang@chinaubi.com");
+		helper.setSubject("测试附件（邮件主题）");//邮件主题.
+		helper.setText("这是邮件内容（有附件哦.）");//邮件内容.
+
+		FileSystemResource resource = new FileSystemResource(
+				new File("C:\\Users\\admin\\Pictures\\22222.jpg")
+		);
+
+		helper.addAttachment(resource.getFilename(), resource);
+		mailSender.send(mimeMessage);
+	}
+
+	@Test
+	public void sendInlineMail() throws Exception {
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+		helper.setFrom("warthur@163.com");//发送者.
+		helper.setTo("wuyongqiang@chinaubi.com");//接收者.
+		helper.setSubject("测试静态资源（邮件主题）");//邮件主题.
+		// 邮件内容，第二个参数指定发送的是HTML格式
+		//说明：嵌入图片<img src='cid:head'/>，其中cid:是固定的写法，而aaa是一个contentId。
+		helper.setText("<body>这是图片：<img src='cid:head' /></body>", true);
+
+		FileSystemResource file = new FileSystemResource(new File("C:\\Users\\admin\\Pictures\\22222.jpg"));
+		helper.addInline("head", file);
+		mailSender.send(mimeMessage);
+	}
+}
