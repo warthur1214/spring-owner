d8d07fa448c494c23173ecc2557b72c5b1763839
warthur1214 <warthur@163.com>
2017-10-27 11:19:09 +0800

    protobuf的配置与使用

---
+++ spring-demo/spring-protobuf/src/main/java/com/warthur/protobuf/ProtoApplication.java
@@ -0,0 +1,12 @@
+package com.warthur.protobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoApplication.class, args);
+	}
+}
