9bc9aa8069e6c3b012e4f4e774ab93125582a9d3
warthur1214 <warthur@163.com>
2017-11-22 10:49:42 +0800

    module结构调整

--- spring-demo/spring-web/src/main/java/com/warthur/spring/web/pojo/SysRole.java
+++ spring-demo/spring-web/src/main/java/com/warthur/spring/web/pojo/SysRole.java
@@ -3,6 +3,7 @@ package com.warthur.spring.web.pojo;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Calendar;
 
 @Entity
 @Table(name = "t_sys_role")
@@ -15,4 +16,14 @@ public class SysRole {
 	private long roleId;
 	@Column(name = "role_name")
 	private String roleName;
+	@Column(name = "update_time",
+			columnDefinition = "datetime not null default current_timestamp on update current_timestamp",
+			insertable = false,
+			updatable = false)
+	private Calendar updateTime;
+	@Column(name = "update_time",
+			columnDefinition = "datetime not null default current_timestamp",
+			insertable = false,
+			updatable = false)
+	private Calendar createTime;
 }
--- spring-demo/spring-web/src/main/java/com/warthur/spring/web/pojo/SysUser.java
+++ spring-demo/spring-web/src/main/java/com/warthur/spring/web/pojo/SysUser.java
@@ -1,13 +1,20 @@
 package com.warthur.spring.web.pojo;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "t_sys_user")
 @Data
-public class SysUser {
+public class SysUser implements UserDetails {
 
 	@Id
 	@GeneratedValue
@@ -17,4 +24,52 @@ public class SysUser {
 	private String userName;
 	@Column(name = "password")
 	private String password;
+	@Column(name = "update_time",
+			columnDefinition = "datetime not null default current_timestamp on update current_timestamp",
+			insertable = false,
+			updatable = false)
+	private Calendar updateTime;
+	@Column(name = "update_time",
+			columnDefinition = "datetime not null default current_timestamp",
+			insertable = false,
+			updatable = false)
+	private Calendar createTime;
+
+	@ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+	private List<SysRole> roles;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> auths = new ArrayList<>();
+		List<SysRole> roles = this.getRoles();
+		for (SysRole role : roles) {
+			auths.add(new SimpleGrantedAuthority(role.getRoleName()));
+		}
+		return auths;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
