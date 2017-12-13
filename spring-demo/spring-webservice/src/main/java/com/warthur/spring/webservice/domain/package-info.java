a1fe7f053144eb79d04cfc4b3e0bd401557ed55c
warthur1214 <warthur@163.com>
2017-12-13 13:41:17 +0800

    spring webservice 练习

---
+++ spring-demo/spring-webservice/src/main/java/com/warthur/spring/webservice/domain/package-info.java
@@ -0,0 +1,9 @@
+//
+// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.7 生成的
+// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// 在重新编译源模式时, 对此文件的所有修改都将丢失。
+// 生成时间: 2017.12.13 时间 10:17:46 AM CST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.warthur.com/webservice/domain", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.warthur.spring.webservice.domain;
